cec4c8051539e0117691e171949c7fdd74b0dc7d
LiuPangYao <a.today.13th.blackdays@gmail.com>
2017-04-22 19:26:18 +0800

    Update MainActivity

--- app/src/main/java/com/example/user/githubsample/MainActivity.java
+++ app/src/main/java/com/example/user/githubsample/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.user.githubsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.edtAccount)
+    EditText edtAccount;
+    @BindView(R.id.edtPassword)
+    EditText edtPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.btnRegister)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 2017-04-21 test
-        // 2017-04-21 test
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnLogin, R.id.btnRegister})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                break;
+            case R.id.btnRegister:
+                break;
+        }
     }
 }
